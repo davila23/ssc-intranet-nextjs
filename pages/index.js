fdecd537a268fcf648b00501fe19e30f76bf952a
Daniel Avila <daniel.avila@rottay.com>
2020-10-27 21:49:30 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Rottay Example <a href="https://www.rottay.com">Next.js!</a>
+          Welcome to <a href="https://www.rottay.com">Rottay!</a>
         </h1>
 
         <p className={styles.description}>
